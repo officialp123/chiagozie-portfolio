8c8bc1c51b94796f1cee1d34402b40da962622e4
Prince Uchenna <96476199+officialp123@users.noreply.github.com>
2025-10-22 11:33:42 +0100

    Update download-cv.tsx

--- src/components/general/download-cv.tsx
+++ src/components/general/download-cv.tsx
@@ -6,7 +6,7 @@ const DownloadCV: React.FC = () => {
   return (
     <Button
       onClick={() =>
-        window?.open("/files/Chiagozie_Uchenna_senior_frontend_Developer.pdf", "_blank")
+        window?.open("/files/Chiagozie_Uchenna_Resume.pdf", "_blank")
       }
     >
       Download CV
@@ -15,3 +15,4 @@ const DownloadCV: React.FC = () => {
 };
 
 export default DownloadCV;
+
