0ff54bacc9f0f4916bc0a7f9f47ffef545f9417d
Prince Uchenna <96476199+officialp123@users.noreply.github.com>
2025-10-22 10:48:30 +0100

    Update tech-details.tsx

--- src/components/data-display/tech-details.tsx
+++ src/components/data-display/tech-details.tsx
@@ -5,16 +5,11 @@ import Typography from '@/components/general/typography';
 import Link from '@/components/navigation/link';
 import ImageWrapper from '@/components/data-display/image-wrapper';
 
-const TechDetails = ({ url, logo, darkModeLogo, label }: TechDetails) => {
+const TechDetails = ({ url, darkModeLogo, label }: TechDetails) => {
   return (
     <div className="flex flex-col items-center gap-2">
       <Link noCustomization href={url} externalLink>
-        <ImageWrapper
-          src={logo}
-          srcForDarkMode={darkModeLogo}
-          alt={label}
-          className="transition-transform duration-300 md:hover:scale-110"
-        />
+        
       </Link>
       <Typography variant="body1">{label}</Typography>
     </div>
@@ -22,3 +17,4 @@ const TechDetails = ({ url, logo, darkModeLogo, label }: TechDetails) => {
 };
 
 export default TechDetails;
+
